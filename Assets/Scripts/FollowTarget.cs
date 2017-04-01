8ad675f91978e29980ef8434cd5dc3761a6215c2
黄世樑MAC <hsl99999@aliyun.com>
2017-04-02 00:49:03 +0800

    测试功能提交

---
+++ Assets/Scripts/FollowTarget.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine; 
+
+public class FollowTarget : MonoBehaviour {  
+
+	public Transform character;   //摄像机要跟随的人物 
+
+	public float smoothTime = 0.01f;  //摄像机平滑移动的时间
+
+	private Vector3 cameraVelocity = Vector3.zero;     
+
+	private Camera mainCamera;  
+
+	private Vector3 cameraPos;
+
+	//主摄像机（有时候会在工程中有多个摄像机，但是只能有一个主摄像机吧）     
+
+	void  Awake ()     
+	{   
+		mainCamera = Camera.main;   
+		cameraPos = mainCamera.transform.localPosition;
+	}  
+
+	void  Update()   
+	{ 
+		transform.position = Vector3.SmoothDamp(transform.position, character.position + cameraPos, ref cameraVelocity, smoothTime);
+	}  
+
+}
---
+++ Assets/Scripts/HeroControlMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroControlMove : MonoBehaviour {
+
+	//定义主角角色控制器  
+	CharacterController hero;
+	public int moveSpeed = 10;
+
+	// Use this for initialization
+	void Start () {
+		hero = GetComponent<CharacterController> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		hero.SimpleMove (Vector3.forward * moveSpeed);
+	}
+}
